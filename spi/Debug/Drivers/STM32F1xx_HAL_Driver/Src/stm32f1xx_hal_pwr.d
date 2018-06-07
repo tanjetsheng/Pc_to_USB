7a8b31733312ed8b209109b79ce4b47f5c5273df
lai zhe hao <lzhao1998@gmail.com>
2018-06-07 17:12:11 +0800

    used cubeMx configure spi

---
+++ spi/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.d
@@ -0,0 +1,68 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/student/tjs/stm32/usb/spi/Inc/stm32f1xx_hal_conf.h \
+ C:/student/tjs/stm32/usb/spi/Inc/main.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/core_cm3.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/student/tjs/stm32/usb/spi/Inc/stm32f1xx_hal_conf.h:
+
+C:/student/tjs/stm32/usb/spi/Inc/main.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/student/tjs/stm32/usb/spi/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h:
